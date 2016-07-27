3676b232755690d3e2c021a9bd045d7a3db8ddf3
Drew Kosta <drewkosta@gmail.com>
2016-07-27 12:12:22 -0700

    complete functional portion

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,20 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[someInstance.size()] = value;
   };
 
   someInstance.dequeue = function() {
+    var result = storage['0'] === undefined ? null : storage['0'];
+    for (var i = 1; i < someInstance.size(); i++) {
+      storage[i - 1] = storage[i]; 
+    }
+    delete storage[someInstance.size() - 1];
+    return result;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
