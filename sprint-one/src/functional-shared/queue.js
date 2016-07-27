36b0f5d7fa648af4b5747544be3fe171b917b454
Drew Kosta <drewkosta@gmail.com>
2016-07-27 12:43:53 -0700

    complete functional-shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,26 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    storage: {}
+  };
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function (val) {
+    this.storage[this.size()] = val;
+  },
+  dequeue: function () {
+    var result = this.storage['0'];
+    for (var i = 1; i < this.size(); i++) {
+      this.storage[i - 1] = this.storage[i];
+    }
+    delete this.storage[this.size() - 1];
+    return result;
+  },
+  size: function () {
+    return Object.keys(this.storage).length;
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,23 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {
+    storage: {}
+  };
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function (val) {
+    this.storage[this.size()] = val;
+  },
+  pop: function () {
+    var result = this.storage[this.size() - 1];
+    delete this.storage[this.size() - 1];
+    return result;
+  },
+  size: function () {
+    return Object.keys(this.storage).length;
+  }
+};
 
 
