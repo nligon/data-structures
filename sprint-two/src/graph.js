035ea7f1e9054c2244b26900ef86790414443cc1
nligon <nathanaelligon@gmail.com>
2016-07-27 19:28:39 -0700

    complete graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,39 +1,57 @@
-
-
 // Instantiate a new graph
 var Graph = function() {
+  this.storage = [];
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.storage[node] = [];
+  return node;
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return !!this.storage[node];
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  for (var i = 0; i < this.storage[node].length; i++) {
+    if (this.storage[node][i]) {
+      this.removeEdge(i, node);
+    }
+  }
+  delete this.storage[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  // return this.storage[toNode].contains(fromNode);
+  return !!this.storage[fromNode][toNode];
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.storage[fromNode][toNode] = true;
+  this.storage[toNode][fromNode] = true;
+  // return false;
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  delete this.storage[toNode][fromNode];
+  delete this.storage[fromNode][toNode];
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var i = 0; i < this.storage.length; i++) {
+    if (this.storage[i]) {
+      cb(i);
+    }
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
