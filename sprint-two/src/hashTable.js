53826ace8573d7195e1cedd0681eb4b9a22707d0
nligon <nathanaelligon@gmail.com>
2016-07-27 20:28:23 -0700

    Implement hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,3 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -7,20 +5,30 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[index] = this._storage[index] || [];
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (this._storage[index][i][0] === k) {
+      this._storage[index][i][1] = v;
+      return;
+    }
+  }
+  this._storage[index].push([k, v]);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (this._storage[index][i][0] === k) {
+      return this._storage[index][i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  for (var i = 0; i < this._storage[index].length; i++) {
+    if (this._storage[index][i][0] === k) {
+      this._storage[index].splice(i, 1);
+    }
+  }
 };
-
-
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
-
-
