d74dadefd6447b992bae6adc5650d83e3816e866
nligon <nathanaelligon@gmail.com>
2016-07-28 21:54:21 -0700

    Implement hash table scaling to double storage when 75% full

--- sprint-two/spec/hashTableSpec.js
+++ sprint-two/spec/hashTableSpec.js
@@ -1,7 +1,14 @@
 describe('hashTable', function() {
   var hashTable;
-  var people = [['Steven', 'Tyler'], ['George', 'Harrison'], ['Mr.', 'Doob'], ['Dr.', 'Sunshine'], ['John', 'Resig'], ['Brendan', 'Eich'], ['Alan', 'Turing']];
-
+  var people = [
+    ['Steven', 'Tyler'],
+    ['George', 'Harrison'],
+    ['Mr.', 'Doob'],
+    ['Dr.', 'Sunshine'],
+    ['John', 'Resig'],
+    ['Brendan', 'Eich'],
+    ['Alan', 'Turing']
+  ];
 
   beforeEach(function() {
     hashTable = new HashTable();
@@ -39,7 +46,9 @@ describe('hashTable', function() {
     var v1 = 'val1';
     var v2 = 'val2';
     var oldHashFunction = window.getIndexBelowMaxForKey;
-    window.getIndexBelowMaxForKey = function() { return 0; };
+    window.getIndexBelowMaxForKey = function() {
+      return 0;
+    };
     hashTable.insert(v1, v1);
     hashTable.insert(v2, v2);
     expect(hashTable.retrieve(v1)).to.equal(v1);
@@ -48,17 +57,20 @@ describe('hashTable', function() {
   });
 
   // (Advanced! Remove the extra "x" when you want the following tests to run)
-  xit ('should double in size when needed', function() {
+  it('should double in size when needed', function() {
     _.each(people, function(person) {
       var firstName = person[0];
       var lastName = person[1];
+      // debugger;
       hashTable.insert(firstName, lastName);
+      // debugger;
       expect(hashTable.retrieve(firstName)).to.equal(lastName);
+      // debugger;
     });
     expect(hashTable._limit).to.equal(16);
   });
 
-  xit ('should halve in size when needed', function() {
+  xit('should halve in size when needed', function() {
     _.each(people, function(person) {
       var firstName = person[0];
       var lastName = person[1];
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,32 +3,65 @@ var HashTable = function() {
   this._storage = LimitedArray(this._limit);
 };
 
+var findStorageLength = function(obj) {
+  var counter = 0;
+  for (var key in obj) {
+    counter++;
+  }
+  return counter;
+};
+
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage[index] = this._storage[index] || [];
-  for (var i = 0; i < this._storage[index].length; i++) {
-    if (this._storage[index][i][0] === k) {
-      this._storage[index][i][1] = v;
+
+  var idx = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[idx] = this._storage[idx] || [];
+  for (var i = 0; i < this._storage[idx].length; i++) {
+    if (this._storage[idx][i][0] === k) {
+      this._storage[idx][i][1] = v;
       return;
     }
   }
-  this._storage[index].push([k, v]);
+  this._storage[idx].push([k, v]);
+
+  // if storage is 75% full, increase limit
+  if (findStorageLength(this._storage) >= this._limit * .75) {
+    this._limit = this._limit * 2;
+    // transfer elements according to new hash
+    this.bakStorage = this._storage;
+    this._storage = {};
+    for (var bucket in this.bakStorage) {
+      if (typeof this.bakStorage[bucket] === 'function') {
+        this._storage[bucket] = this.bakStorage[bucket];
+      } else {
+        // get bucket index for new hash
+        var idx = getIndexBelowMaxForKey(this.bakStorage[bucket][0][0], this._limit);
+        this._storage[idx] = this._storage[idx] || [];
+        // iterate over all tuples in bakBucket
+        for (var tuple = 0; tuple < this.bakStorage[bucket].length; tuple++) {
+          this._storage[idx].push(this.bakStorage[bucket][0]);
+        }
+      }
+    }
+    delete this.bakStorage;
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  for (var i = 0; i < this._storage[index].length; i++) {
-    if (this._storage[index][i][0] === k) {
-      return this._storage[index][i][1];
+  var idx = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage[idx] !== undefined) {
+    for (var i = 0; i < this._storage[idx].length; i++) {
+      if (this._storage[idx][i][0] === k) {
+        return this._storage[idx][i][1];
+      }
     }
   }
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  for (var i = 0; i < this._storage[index].length; i++) {
-    if (this._storage[index][i][0] === k) {
-      this._storage[index].splice(i, 1);
+  var idx = getIndexBelowMaxForKey(k, this._limit);
+  for (var i = 0; i < this._storage[idx].length; i++) {
+    if (this._storage[idx][i][0] === k) {
+      this._storage[idx].splice(i, 1);
     }
   }
 };
